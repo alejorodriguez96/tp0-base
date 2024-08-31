eb477ef161f478541570b26e022d7d028f82f1f1
Alejo Rodriguez <atrodriguez@fi.uba.ar>
2024-08-31 02:12:25 -0300

    add generate compose scripts

---
+++ scripts/generar_compose.py
@@ -0,0 +1,123 @@
+from typing import TextIO
+import sys
+
+NETWORK_NAME = "testing_net"
+NETWORK_SUBNET = "172.25.125.0/24"
+NETWORK = {
+    NETWORK_NAME: {
+        "ipam": {
+            "driver": "default",
+            "config": [
+                {
+                    "subnet": NETWORK_SUBNET
+                }
+            ]
+        }
+    }
+}
+
+SERVER_SERVICE = {
+    "container_name": "server",
+    "image": "server:latest",
+    "entrypoint": "python3 /main.py",
+    "environment": [
+        "PYTHONUNBUFFERED=1",
+        "LOGGING_LEVEL=DEBUG"
+    ],
+    "networks": [
+        NETWORK_NAME
+    ]
+}
+
+#   client1:
+#     container_name: client1
+#     image: client:latest
+#     entrypoint: /client
+#     environment:
+#       - CLI_ID=1
+#       - CLI_LOG_LEVEL=DEBUG
+#     networks:
+#       - testing_net
+#     depends_on:
+#       - server
+
+def generar_client_service(nombre: str):
+    """Genera la configuración para ejecutar un servicio de cliente.
+    
+    Args:
+        nombre (int): Nombre a asignarle al servicio.
+
+    Returns:
+        dict: Configuración del servicio.
+    """
+    return {
+        "container_name": nombre,
+        "image": "client:latest",
+        "entrypoint": "/client",
+        "environment": [
+            "CLI_ID=1",
+            "CLI_LOG_LEVEL=DEBUG"
+        ],
+        "networks": [
+            NETWORK_NAME
+        ],
+        "depends_on": [
+            "server"
+        ]
+    }
+
+def escribir_yaml(compose: dict, file: TextIO, indent: int = 0) -> None:
+    """Escribe un archivo YAML con la configuración de un docker-compose.
+    
+    Args:
+        compose (dict): Configuración del docker-compose.
+        file (TextIO): Archivo a escribir.
+    """
+    for key, value in compose.items():
+        file.write("  " * indent)
+        file.write(f"{key}: ")
+        if isinstance(value, dict):
+            file.write("\n")
+            escribir_yaml(value, file, indent + 1)
+        elif isinstance(value, list):
+            file.write("\n")
+            for item in value:
+                file.write("  " * (indent + 1))
+                file.write(f"- {item}\n")
+        else:
+            file.write(f"{value}\n")
+        file.write("\n")
+
+
+def generar_compose(path_archivo: str, clientes: int):
+    """
+    Genera un archivo docker-compose.yml con la configuración
+    para ejecutar un servidor y varios clientes.
+    
+    Args:
+        path_archivo (str): Nombre del archivo a generar.
+        clientes (int): Número de clientes a agregar al compose.
+    """
+    services = {
+        "server": SERVER_SERVICE
+    }
+    for i in range(clientes):
+        nombre = f"client{i + 1}"
+        services[nombre] = generar_client_service(nombre)
+    compose = {
+        "name": "tp0",
+        "services": services,
+        "networks": NETWORK,
+    }
+    with open(path_archivo, "w", encoding="utf-8") as f:
+        escribir_yaml(compose, f)
+
+if __name__ == "__main__":
+    try:
+        path = sys.argv[1]
+        cantidad = int(sys.argv[2])
+        generar_compose(path, cantidad)
+    except IndexError:
+        print("Uso: python3 generar_compose.py <path al archivo de destino> <cantidad>")
+    except ValueError:
+        print("La cantidad debe ser un número entero.")
