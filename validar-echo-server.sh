f96e52229ac7a6662cd6dbf93d1ea642d9f91cd6
Alejo Rodriguez <atrodriguez@fi.uba.ar>
2024-08-31 02:59:51 -0300

    add netcat to server image and validate echo script

---
+++ validar-echo-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+RESPONSE=$(docker exec -i server bash -c 'echo -n "Hola mundo" | nc server 12345')
+
+if [ "$RESPONSE" == "Hola mundo" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
