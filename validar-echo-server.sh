88b8eb7215d4df80a0a3d6df99cd041c86d3d3d6
Alejo Rodriguez <atrodriguez@fi.uba.ar>
2024-09-01 23:28:19 -0300

    make validate script compatible with sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 RESPONSE=$(docker exec -i server bash -c 'echo -n "Hola mundo" | nc server 12345')
 
-if [ "$RESPONSE" == "Hola mundo" ]; then
+if [ "$RESPONSE" = "Hola mundo" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
