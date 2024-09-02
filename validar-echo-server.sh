07c9681502fae67d4afd826be8bfede6c251a6d2
Alejo Rodriguez <atrodriguez@fi.uba.ar>
2024-09-02 00:12:32 -0300

    rm netcat install form server and use nc docker image

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RESPONSE=$(docker exec -i server bash -c 'echo -n "Hola mundo" | nc server 12345')
+RESPONSE=$(docker run --rm -i --network tp0_testing_net --entrypoint sh gophernet/netcat -c 'echo "Hola mundo" | nc server 12345')
 
 if [ "$RESPONSE" = "Hola mundo" ]; then
     echo "action: test_echo_server | result: success"
